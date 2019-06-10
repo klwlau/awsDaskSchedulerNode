ee939d60b5456cff46dc4a2a95b1b8d32a4c183c
Wilson.L <klwilsonlau@gmail.com>
2019-06-10 18:00:27 +0800

    update

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -x
 
+cd ~
 mkdir daskData
 cd daskData
 
 /usr/bin/tmux new-session -d -s jupyter
-/usr/bin/tmux send-keys -t jupyter "/home/ubuntu/anaconda/bin/jupyter lab --ip=0.0.0.0 --allow-root" C-m
+/usr/bin/tmux send-keys -t jupyter "/home/ubuntu/anaconda/bin/jupyter lab --ip=0.0.0.0" C-m
 
 /usr/bin/tmux new-session -d -s dask_s
 /usr/bin/tmux send-keys -t dask_s "/home/ubuntu/anaconda/bin/dask-scheduler" C-m
